d7e0547a7f08d191335504358d2ae609ae3a36fa
as <>
2021-09-03 16:18:22 +0900

    image配置、クリックしたらColliderNameを呼ぶ

---
+++ Assets/Script/PaintBucket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintBucket : MonoBehaviour
+{
+    public Color[] colorList;
+    public Color curColor;
+    public int colorCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curColor = colorList[colorCount];
+
+        Vector3 pos = Input.mousePosition;
+        pos.z = 10.0f;
+        var ray = Camera.main.ScreenToWorldPoint(pos);
+        RaycastHit2D hit = Physics2D.Raycast(ray, -Vector2.up);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (hit.collider != null)
+            {
+                SpriteRenderer sp = hit.collider.gameObject.GetComponent<SpriteRenderer>();
+                Debug.Log(hit.collider.name);
+            }
+        }
+    }
+
+    
+}
